c838c9b4b6272e426e59c709912be6da0cbcbf43
Chiramert <fortjiramet10@gmail.com>
2024-12-03 00:44:42 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int i = 0;
+    int integer[100];
+    int x = -1;
+
+    while (x != 0) {
+        cout << "Enter an integer: ";
+        cin >> x;
+        if (x != 0){integer[i] = x;
+        i++;}
+    }
+    int Even = 0,odd = 0;
+    for (int limit = 0; limit < i; limit++) {
+        if(integer[limit] % 2 == 0){
+            Even++;
+        }
+        else {
+            odd++;
+        }
+    }
+    cout << "#Even numbers = " << Even << "\n";
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
